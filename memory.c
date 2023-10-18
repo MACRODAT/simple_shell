a97526c2af4900b276f0bb883a05be334e64a4c2
MACRODAT <macrodatservices@gmail.com>
2023-10-18 01:59:45 +0100

    Changes to the file

--- memory.c
+++ memory.c
@@ -1,10 +1,17 @@
 #include "shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
+ * bfree - Free memory allocated for a pointer and set it to NULL.
  *
- * Return: 1 if freed, otherwise 0.
+ * This function checks if the provided pointer 'ptr' is not NULL and if
+ * '*ptr' points to allocated memory. If these conditions are met, it frees the
+ * memory, sets '*ptr' to NULL, and returns 1.
+ * If the conditions are not met, it
+ * returns 0.
+ *
+ * @ptr: A double pointer to the pointer whose memory needs to be freed.
+ *
+ * Return: 1 if memory is freed and set to NULL, 0 otherwise.
  */
 int bfree(void **ptr)
 {
