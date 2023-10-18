17728385efdf4d697dbbe4a849f3303a34d90e23
MACRODAT <macrodatservices@gmail.com>
2023-10-18 21:53:02 +0100

    Changes to the file

--- parser.c
+++ parser.c
@@ -23,49 +23,6 @@ int executable(shell_data_ *sh_data, char *path)
 }
 
 /**
- * where_is - dew
- * @sh_data: de
- * @_loc_st_2: edw
- * @cmd: ewd
- *
- * Return: Tdwe
- */
-char *where_is(shell_data_ *sh_data, char *_loc_st_2, char *cmd)
-{
-	int i = 0, cur_pso = 0;
-	char *loc;
-
-	if (!_loc_st_2)
-		return (NULL);
-	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
-	{
-		if (executable(sh_data, cmd))
-			return (cmd);
-	}
-	while (1)
-	{
-		if (!_loc_st_2[i] || _loc_st_2[i] == ':')
-		{
-			loc = dup_chars(_loc_st_2, cur_pso, i);
-			if (!*loc)
-				_strcat(loc, cmd);
-			else
-			{
-				_strcat(loc, "/");
-				_strcat(loc, cmd);
-			}
-			if (executable(sh_data, loc))
-				return (loc);
-			if (!_loc_st_2[i])
-				break;
-			cur_pso = i;
-		}
-		i++;
-	}
-	return (NULL);
-}
-
-/**
  * dup_chars - wdc
  * @_loc_st_2: we
  * @start: wed
